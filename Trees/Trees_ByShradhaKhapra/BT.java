14a93d93694a3b7619f0deac6905554b8466d344
Kaushiki <kaushikichoudhary3@gmail.com>
2025-12-06 13:37:42 +0000

    initial commit

--- Trees/Trees_ByShradhaKhapra/BT.java
+++ Trees/Trees_ByShradhaKhapra/BT.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BT{
 
     public static void main(String[] args) {
@@ -5,8 +8,18 @@ public class BT{
 
         BT tree = new BT();
         Node root = tree.buildTree(preorderSeq);
+        System.out.println("Preorder traversal");
         tree.preorderTraversal(root);
-
+        System.out.println();
+        System.out.println("Inorder traversal");
+        tree.inorderTraversal(root);
+        System.out.println();
+        System.out.println("Postorder traversal");
+        tree.postorderTraversal(root);
+        System.out.println();
+        System.out.println("Level order traversal");
+        tree.levelorderTraversal(root);
+        System.out.println();
     }
 
     public static class Node{
@@ -20,16 +33,7 @@ public class BT{
         }
     }
 
-    public void preorderTraversal(Node node){
-        
-        if(node == null){
-            return;
-        }
-
-        System.out.println(node.data);
-        preorderTraversal(node.leftSubTree);
-        preorderTraversal(node.rightSubTree);
-    }
+    // Building a binary tree
 
     int index = -1;
     public Node buildTree(int[] preorderSeq){
@@ -52,6 +56,80 @@ public class BT{
     }
 
 
+    // ... PREORDER TRAVERSAL
+
+    public void preorderTraversal(Node node){
+        
+        if(node == null){
+            return;
+        }
+
+        System.out.print(node.data + " ");
+
+        preorderTraversal(node.leftSubTree);
+        preorderTraversal(node.rightSubTree);
+    }
+
+
+    // .... INORDER TRAVERSAL
+
+    public void inorderTraversal(Node node){
+        
+        if(node == null){
+            return;
+        }
+
+        inorderTraversal(node.leftSubTree);
+        System.out.print(node.data + " ");
+        inorderTraversal(node.rightSubTree);
+    }
+
+
+    // .... POSTORDER TRAVERSAL
+
+    public void postorderTraversal(Node node){
+        
+        if(node == null){
+            return;
+        }
+
+        postorderTraversal(node.leftSubTree);
+        postorderTraversal(node.rightSubTree);
+        System.out.print(node.data + " ");
+    }
+
+
+    // LEVEL ORDER TRAVERSAL
+
+    public void levelorderTraversal(Node node){
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        q.add(null);
+        
+        while(!q.isEmpty()){
+            Node current  = q.poll();
+            if(current == null){
+                if(!q.isEmpty()){
+                    System.out.println();
+                    q.add(null);
+                }
+                else{
+                    break;
+                }
+
+                continue;
+            }
+            System.out.print(current.data + " ");
+
+            if(current.leftSubTree != null){
+                q.add(current.leftSubTree);
+            }
+
+            if(current.rightSubTree != null){
+                q.add(current.rightSubTree);
+            }
+        }
+    }
 
 
 
