816708034c39466d6591b6cbd036a5d8178f93b5
Kaushiki <kaushikichoudhary3@gmail.com>
2025-12-14 07:26:03 +0000

    BuildBst, Insert, Traversal, Search, Delete

--- Trees/BinarySearchTree/BuildBST.java
+++ Trees/BinarySearchTree/BuildBST.java
@@ -63,12 +63,73 @@ public class BuildBST{
 
     }
 
+
+    // Delete a given node in the BST
+    Node delete(Node root, int target){
+        if(root == null){
+            return null;
+        }
+
+        if(root.data > target){
+            root.left = delete(root.left, target);
+            return root;
+        }
+        if(root.data < target){
+            root.right = delete(root.right, target);
+            return root;
+        }
+        else{
+            // root.data == target ( iska mtlb hai ki hame yahi node delete krni hai)
+            // pr idhar 3 cases ban skte hain
+
+            // leaf node ko delete kr rhe hain
+            if(root.right == null && root.left == null){
+                //delete root;
+                return null;
+            }
+
+            // ya fir aisi node jiska sirf 1 child hai (either left or right)
+            if(root.right == null){
+                Node temp = root.left;
+                return temp;
+            }
+            if(root.left == null){
+                Node temp = root.right;
+                return temp;
+            }
+
+            // ya fir aisi node jiske 2 child hain
+            else{
+            Node child = root.left;
+            Node parent = root;
+
+            while(child.right != null){
+                parent=child;
+                child=child.right;
+            }
+
+            if(root == parent){
+                child.right = root.right;
+                return child;
+            }
+            else{
+                parent.right=child.left;
+                child.left=root.left;
+                child.right=root.right;
+                return child;
+            }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {6,3,17,5,11,18,2,1,20,14};
         BuildBST tree = new BuildBST();
         Node root = tree.buildTree(arr);
         tree.inorderTraversal(root);
         System.out.println(tree.search(root, 18));
+        Node UpdatedRoot = tree.delete(root, 1);
+        tree.inorderTraversal(UpdatedRoot);
     }
 
 }
